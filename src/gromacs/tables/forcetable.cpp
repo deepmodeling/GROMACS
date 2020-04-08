fa6c5303aa64612517527901252a9eb7a4ff7317
Mark Abraham <mark.j.abraham@gmail.com>
2020-04-08 10:50:27 +0200

    Fixed ENCAD repulsion table
    This is unused in normal GROMACS

--- src/gromacs/tables/forcetable.cpp
+++ src/gromacs/tables/forcetable.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019,2020, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1027,8 +1027,8 @@ static void fill_table(t_tabledata* td, int tp, const interaction_const_t* ic, g
             case etabLJ12Encad:
                 if (r < rc)
                 {
-                    Vtab = -(r6 - 6.0 * (rc - r) * rc6 / rc - rc6);
-                    Ftab = -(6.0 * r6 / r - 6.0 * rc6 / rc);
+                    Vtab = -(r12 - 12.0 * (rc - r) * rc12 / rc - rc12);
+                    Ftab = -(12.0 * r12 / r - 12.0 * rc12 / rc);
                 }
                 else /* r>rc */
                 {
