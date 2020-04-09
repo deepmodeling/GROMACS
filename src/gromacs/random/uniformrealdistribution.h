d0ceb3de531c65e1fb641b29111b1349ef9aba46
Szilárd Páll <pall.szilard@gmail.com>
2020-04-09 23:34:36 +0200

    Fix clang 10 -Wimplicit-int-float-conversion warnings

--- src/gromacs/random/uniformrealdistribution.h
+++ src/gromacs/random/uniformrealdistribution.h
@@ -109,7 +109,7 @@ RealType generateCanonical(Rng& g)
     const uint64_t range    = Rng::max() - Rng::min() + uint64_t(1);
     uint64_t       log2R    = (range == 0) ? std::numeric_limits<uint64_t>::digits : log2I(range);
     uint64_t       k        = realBits / log2R + (realBits % log2R != 0) + (realBits == 0);
-    RealType       r        = Rng::max() - Rng::min() + RealType(1);
+    RealType       r        = static_cast<RealType>(Rng::max() - Rng::min()) + RealType(1);
     RealType       s        = g() - Rng::min();
     RealType       base     = r;
     RealType       result;
