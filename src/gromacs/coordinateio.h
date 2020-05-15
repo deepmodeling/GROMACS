e3204ee249fca61a57c915ceff0b59f995ea24ca
Paul Bauer <paul.bauer.q@gmail.com>
2020-05-15 12:54:39 +0200

    Add high level docs
    Change-Id: I18dad59c8b2a758adabc97f4d41018601e01ec16

--- src/gromacs/coordinateio.h
+++ src/gromacs/coordinateio.h
@@ -46,7 +46,7 @@
  * - \ref module_options
  * - \ref module_selection
  *
- * <H3>Overview</H3>
+ * <H3>Overview of coordinate file output</H3>
  * The methods in coordinateio provide the infrastructure to perform operations on coordinate data files
  * and structures during data analysis. It implements ways to change the information
  * in coordinate data structures as well as checking that both input data and output
@@ -133,6 +133,87 @@
  * in changing a minimal set of t_trxframe data.
  * \endif
  *
+ * <H3>Overview of coordinate modification (coordinate converter) routines</H3>
+ * The methods based on the IFrameConverter interface provide ways to change
+ * the coordinates found in input files according to a user or program specified
+ * pattern that fulfils a specified requirement. The basic adapter implements a single
+ * method that performs one operation on a coordinate file, which provides a guarantee
+ * on the produced coordinates. Those guarantees should be simple enough so that they
+ * can be fulfilled by the single operation, and avoid complex invocation patterns.
+ *
+ * <H3>Combination of different frame converters</H3>
+ * Several individual converters can be combined and executed in sequence to provide
+ * more complex operations. Here, each individual converter can provide part of a complex
+ * guarantee requested for the final output structure, depending on the combination
+ * and sequence of the adapters. Implementers need to take care to consider if an
+ * adapter added to a particular sequence will invalidate a previous guarantee, to
+ * make sure there is a way to avoid operations unknowingly invalidating each other.
+ *
+ * <H3>Data handling for frame conversion methods</H3>
+ * Methods that use the IFrameConverter based registration and chaining method do not
+ * need handle their own data. The registration method provides data storage for the
+ * modified coordinates and returns a final, modified t_trxframe datastructure with
+ * the method owned coordinates. No changes are applied to the velocity and force
+ * fields (if present), with eventually present data used in the new datastructure.
+ *
+ * Methods that implement single converters without the registration machinery need to
+ * implement their own memory handling.
+ *
+ * <H3>FrameAdapers</H3>
+ * Each class implemented on top of the IFrameConverter interface implements each own
+ * convertFrame method that performs its operation on the coordinates in the t_trxframe
+ * input. As well as this, the method should report which kind of guarantee is provided
+ * by the method, as reported by the guarantee method.
+ *
+ * \msc
+   wordwraparcs=true,
+   hscale="2";
+
+   analysistool,
+   requirements [ label="Requirements to run method" ],
+   container [ label="FrameConverterChain" ],
+   frameconverters [ label="FrameConverters" ];
+
+   analysistool => builder [ label="Requests to run analysis method" ];
+   analysistool => builder [ label="Specifies required modifications needed" ];
+   requirements => container [ label="Add methods needed for modifications" ];
+   container => frameconverters [ label="Adds each in converter" ];
+   frameconverters => container [ label="Adds to final guarantees provided by chained methods" ];
+   container => analysistool [ label="Reports on final set of method that provide the needed changes" ];
+   analysistool box analysistool [ label="Can now run the analysis with the preprocessing from the frameconverters" ];
+ * \endmsc
+ *
+ *
+ * \msc
+   wordwraparcs=true,
+   hscale="2";
+
+   analysistool,
+   analysisloop,
+   inputcoordinates,
+   frameconverterholder [ label="Chain of modification tools" ],
+   frameconverters [ label="FrameConverters" ];
+
+   --- [ label="Setup of frameconverters complete, analysis phase begins" ];
+    analysistool   => analysisloop [ label="Starts iteration over frames" ];
+    analysisloop   => frameconvertholder [ label="Provides initial unmodified coordinates" ];
+    frameconverterholder => frameconverters [ label="Each successive converter modifies coordinates" ];
+    frameconverters => frameconverterholder [ label="Return coordinates from modification" ];
+    frameconverterholder => analysisloop [ label="Return final coordinates to analysis tool for analysis" ];
+    analysisloop box analysisloop [ label="Iterates over frames" ];
+    --- [ label="Analysis complete, object is destructed and files are closed" ];
+
+ *  \endmsc
+ *
+ * \if libapi
+ * <H3>Preparing new FrameConverters</H3>
+ *
+ * If additional methods are needed to perform modify coordinate data,
+ * new FrameConverters can be written that again implement the IFrameConverter interface.
+ * The new method should follow the approach of the other modules that are present
+ * in performing single modifications on the coordinates.
+ * \endif
+
  * \author Paul Bauer <paul.bauer.q@gmail.com>
  */
 /*! \file
