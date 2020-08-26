fe8c2219a762cf05d81502171d54ca8a1db1e567
Artem Zhmurov <zhmurov@gmail.com>
2020-08-26 13:19:10 +0300

    Update the comment to setDevice(...) function

--- src/gromacs/hardware/device_management.h
+++ src/gromacs/hardware/device_management.h
@@ -133,6 +133,10 @@ std::vector<int> getCompatibleDevices(const std::vector<std::unique_ptr<DeviceIn
 
 /*! \brief Set the active GPU
  *
+ * This sets the device for which the device information is passed active. Essential in CUDA, where
+ * the device buffers and kernel launches are not connected to the device context. In OpenCL, makes
+ * performance adjustment, that are specific to the device being set.
+ *
  * \param[in] deviceInfo Information on the device to be set.
  *
  * Issues a fatal error for any critical errors that occur during
