6c427d55ecb7b813f5d8091d17523c6f8be5f7b6
Andrey Alekseenko <al42and@gmail.com>
2020-12-08 22:02:07 +0100

    Undo useless change of gmx::RVec

--- src/gromacs/math/vectypes.h
+++ src/gromacs/math/vectypes.h
@@ -102,7 +102,7 @@ public:
     //! Constructs default (uninitialized) vector.
     BasicVector() {}
     //! Constructs a vector from given values.
-    constexpr BasicVector(ValueType x, ValueType y, ValueType z) : x_{ x, y, z } {}
+    BasicVector(ValueType x, ValueType y, ValueType z) : x_{ x, y, z } {}
     /*! \brief
      * Constructs a vector from given values.
      *
