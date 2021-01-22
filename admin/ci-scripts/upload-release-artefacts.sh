de0a2e24192090b234a4b7fc84cefa2a4dd2a41f
Paul Bauer <paul.bauer.q@gmail.com>
2021-01-22 15:38:35 +0100

    Try to make spellcheck happy

--- admin/ci-scripts/upload-release-artefacts.sh
+++ admin/ci-scripts/upload-release-artefacts.sh
@@ -23,12 +23,12 @@ UPLOAD_REMOTE_WWW=manualbot@manual.gromacs.org
 
 if [[ "${CI_COMMIT_REF_NAME}" != "master" && "${CI_COMMIT_REF_NAME}" != "release-2021" && "${CI_COMMIT_REF_NAME}" != "release-2020" ]] ; then
     echo "Not running for any recognized branch, not uploading to the real locations"
-    UPLOAD_LOCATION_FTP=${UPLOAD_LOCATION_FTP}/.ci-test
-    UPLOAD_LOCATION_WWW=${UPLOAD_LOCATION_WWW}/.ci-test
+    UPLOAD_LOCATION_FTP="${UPLOAD_LOCATION_FTP}/.ci-test"
+    UPLOAD_LOCATION_WWW="${UPLOAD_LOCATION_WWW}/.ci-test"
 elif [[ "${GROMACS_RELEASE}" != "true" ]] ; then
     echo "Not running a true release build, not uploading to the real locations"
-    UPLOAD_LOCATION_FTP=${UPLOAD_LOCATION_FTP}/.ci-test
-    UPLOAD_LOCATION_WWW=${UPLOAD_LOCATION_WWW}/.ci-test
+    UPLOAD_LOCATION_FTP="${UPLOAD_LOCATION_FTP}/.ci-test"
+    UPLOAD_LOCATION_WWW="${UPLOAD_LOCATION_WWW}/.ci-test"
 fi
 
 echo "Running upload to FTP (${UPLOAD_LOCATION_FTP}), WWW (${UPLOAD_LOCATION_WWW}) servers"
@@ -37,7 +37,7 @@ echo "We are uploading files for this version: ${VERSION}"
 # Get files for uploading the manual front page
 MANUAL_PAGE_REPO=manual-front-page
 if [[ -d "${MANUAL_PAGE_REPO}" ]]; then
-    rm -rf "${MANUAL_PAGE_REPO}"
+    rm -rf ${MANUAL_PAGE_REPO}
 fi
 git clone --depth=1 git@gitlab.com:gromacs/deployment/manual-front-page.git
 
@@ -53,17 +53,17 @@ originalpwd="${PWD}"
     
     upload="rsync -rlptvP --chmod=u+rwX,g+rwX,o+rX"
     deploymentlocation="${UPLOAD_REMOTE_WWW}:${UPLOAD_LOCATION_WWW}"
-    website_loc=${BUILD_DIR}/docs/html/
+    website_loc="${BUILD_DIR}/docs/html/"
     if [[ ! -d "${website_loc}" ]] ; then
         echo "Error Can't find the webpage files"
         exit 1
     fi
     # we always fail save
     if [[ "${DRY_RUN}" == "false" ]] ; then
-        $upload $website_loc/* $website_loc/.[a-z]* $deploymentlocation/${VERSION}/
+        ${upload} ${website_loc}/* ${website_loc}/.[a-z]* ${deploymentlocation}/${VERSION}/
     fi
     echo "done upload"
-    cp "${website_loc}/manual-${VERSION}.pdf" "${originalpwd}"
+    cp ${website_loc}/manual-${VERSION}.pdf ${originalpwd}
 )
 
 
@@ -76,9 +76,9 @@ originalpwd="${PWD}"
     md5sum "${regressiontests_tarball}"
     # we always fail save
     if [[ "${DRY_RUN}" == "false" ]] ; then
-        $upload $source_tarball $destination/gromacs/
-        $upload manual-${VERSION}.pdf $destination/manual/
-        $upload $regressiontests_tarball $destination/regressiontests/
+        ${upload} ${source_tarball} ${destination}/gromacs/
+        ${upload} manual-${VERSION}.pdf ${destination}/manual/
+        ${upload} ${regressiontests_tarball} ${destination}/regressiontests/
     fi
 )
 
@@ -89,7 +89,7 @@ originalpwd="${PWD}"
     make html
     # we always fail save
     if [[ "${DRY_RUN}" == "false" ]] ; then
-        $upload _build/html/ $deploymentlocation/ --exclude _sources --exclude .buildinfo --exclude objects.inv
+        ${upload} _build/html/ ${deploymentlocation}/ --exclude _sources --exclude .buildinfo --exclude objects.inv
     fi
 )
 
