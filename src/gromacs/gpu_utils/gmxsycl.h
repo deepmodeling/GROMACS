7db0af56c2af37ebb588b5f5b89334eccb8e72d8
Andrey Alekseenko <al42and@gmail.com>
2021-01-22 20:01:35 +0300

    Expose Intel SYCL extensions in a manner compatible with SYCL2020 (!928)

--- src/gromacs/gpu_utils/gmxsycl.h
+++ src/gromacs/gpu_utils/gmxsycl.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2020, by the GROMACS development team, led by
+ * Copyright (c) 2020,2021, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -38,13 +38,16 @@
  *
  * SYCL headers use symbol DIM as a template parameter, which gets broken by macro DIM defined
  * in gromacs/math/vectypes.h. Here, we include the SYCL header while temporary undefining this macro.
+ * The issue for Intel LLVML compiler: https://github.com/intel/llvm/issues/2981
+ *
+ * Different compilers, at the time of writing, have different names for some of the proposed features
+ * of the SYCL2020 standard. For uniformity, they are all aliased in our custom sycl_2020 namespace.
  *
  * \inlibraryapi
  */
 
 #ifndef GMX_GPU_UTILS_GMXSYCL_H
 #define GMX_GPU_UTILS_GMXSYCL_H
-
 
 #ifdef DIM
 #    if DIM != 3
@@ -57,5 +60,52 @@
 #else
 #    include <CL/sycl.hpp>
 #endif
+
+/* Exposing Intel-specific extensions in a manner compatible with SYCL2020 provisional spec.
+ * Despite ICPX (up to 2021.1.2 at the least) having SYCL_LANGUAGE_VERSION=202001,
+ * some parts of the spec are still in private namespace (sycl::intel or sycl::ONEAPI, depending
+ * on the version), and some functions have different names. To make things easier to upgrade
+ * in the future, this thin layer is added.
+ * */
+namespace sycl_2020
+{
+namespace detail
+{
+#if defined(__SYCL_COMPILER_VERSION) // Intel SYCL compiler
+// Confirmed to work for 2021.1-beta10 (20201005), 2021.1.1 (20201113), 2021.1.2 (20201214).
+namespace origin = cl::sycl::ONEAPI;
+#elif defined(__HIPSYCL__)
+namespace origin = cl::sycl;
+#else
+#    error "Unsupported version of Intel SYCL compiler"
+#endif
+} // namespace detail
+
+using detail::origin::memory_order;
+using detail::origin::memory_scope;
+using detail::origin::plus;
+using detail::origin::sub_group;
+
+#if defined(__SYCL_COMPILER_VERSION)
+using detail::origin::atomic_ref;
+template<typename... Args>
+bool group_any_of(Args&&... args)
+{
+    return detail::origin::any_of(std::forward<Args>(args)...);
+}
+template<typename... Args>
+auto group_reduce(Args&&... args) -> decltype(detail::origin::reduce(std::forward<Args>(args)...))
+{
+    return detail::origin::reduce(std::forward<Args>(args)...);
+}
+#elif defined(__HIPSYCL__)
+// No atomic_ref in hipSYCL yet (2021-01-20)
+using detail::origin::group_any_of;
+using detail::origin::group_reduce;
+#else
+#    error "Unsupported SYCL compiler"
+#endif
+
+} // namespace sycl_2020
 
 #endif
