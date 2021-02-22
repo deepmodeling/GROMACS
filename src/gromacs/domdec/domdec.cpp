fb745bd62aa90455045dff358aea936b1c2ad4dd
ejjordan <ejjordan@kth.se>
2021-02-22 10:17:35 +0100

    address review comments

--- src/gromacs/domdec/domdec.cpp
+++ src/gromacs/domdec/domdec.cpp
@@ -228,9 +228,8 @@ gmx_domdec_zones_t* domdec_zones(gmx_domdec_t* dd)
 
 std::vector<std::tuple<gmx::RVec, gmx::RVec>> domdecZoneSizes(const gmx_domdec_zones_t& domdecZones)
 {
-    std::vector<std::tuple<gmx::RVec, gmx::RVec>> zoneSizes;
-    zoneSizes.resize(domdecZones.n);
-    for (int zone = 1; zone < domdecZones.n; zone++)
+    std::vector<std::tuple<gmx::RVec, gmx::RVec>> zoneSizes(domdecZones.n);
+    for (int zone = 0; zone < domdecZones.n; zone++)
     {
         gmx::RVec c0, c1;
         for (int d = 0; d < DIM; d++)
