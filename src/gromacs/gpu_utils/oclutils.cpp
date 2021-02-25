9aca2b7f93fb7a3b3f2459e85e265021ac3858b9
Andrey Alekseenko <al42and@gmail.com>
2021-02-25 18:08:32 +0300

    Remove duplicate \brief

--- src/gromacs/gpu_utils/oclutils.cpp
+++ src/gromacs/gpu_utils/oclutils.cpp
@@ -2,7 +2,7 @@
  * This file is part of the GROMACS molecular simulation package.
  *
  * Copyright (c) 2014,2015,2016,2017,2018 by the GROMACS development team.
- * Copyright (c) 2019,2020, by the GROMACS development team, led by
+ * Copyright (c) 2019,2020,2021, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -53,7 +53,7 @@
 #include "gromacs/utility/fatalerror.h"
 #include "gromacs/utility/smalloc.h"
 
-/*! \brief \brief Allocates nbytes of host memory. Use ocl_free to free memory allocated with this function.
+/*! \brief Allocates nbytes of host memory. Use ocl_free to free memory allocated with this function.
  *
  *  \todo
  *  This function should allocate page-locked memory to help reduce D2H and H2D
