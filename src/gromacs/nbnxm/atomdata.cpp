8a8f491e49b5c4e3cbf1e3da10d3f88ea1757426
Y Yan <yanyx@dp.tech>
2021-04-20 15:45:54 +0800

    debug more

--- src/gromacs/nbnxm/atomdata.cpp
+++ src/gromacs/nbnxm/atomdata.cpp
@@ -1110,7 +1110,7 @@ void nbnxn_atomdata_setAB(nbnxn_atomdata_t*     nbat,
     nbnxn_atomdata_set_chargesAB(nbat, gridSet, atomChargesA, atomChargesB);
 
     /* This must be done after masking types for FEP */
-    nbnxn_atomdata_set_ljcombparams(&params, nbat->XFormat, gridSet);
+    nbnxn_atomdata_set_ljcombparamsAB(&params, nbat->XFormat, gridSet);
 
     nbnxn_atomdata_set_energygroups(&params, gridSet, atomInfo);
 }
