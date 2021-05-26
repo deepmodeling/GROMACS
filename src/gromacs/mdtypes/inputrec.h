f67fe8509c4278ee3e9ffdb17ecc5cacb9133761
Chang Junhan <1700011741@pku.edu.cn>
2021-05-27 07:52:27 +0800

    bugfix

--- src/gromacs/mdtypes/inputrec.h
+++ src/gromacs/mdtypes/inputrec.h
@@ -95,6 +95,7 @@ struct t_sits
     char* nk_rest_file;   //记录最后一帧nk的文件
     char* norm_traj_file; //记录log_norm变化的文件
     char* norm_rest_file; //记录最后一帧log_norm的文件
+    char* pk_traj_file;
     int nst_sits_enerd_out;
     char* sits_enerd_out;
 };
