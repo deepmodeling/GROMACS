a677a5a819e45f1b8030b9e9d729e344dc78e9d3
AnguseZhang <529133328@qq.com>
2021-08-01 19:56:05 +0800

    fixup: remove init inversed indices when NB is computed by CPU

--- src/gromacs/mdlib/sim_util.cpp
+++ src/gromacs/mdlib/sim_util.cpp
@@ -1407,7 +1407,10 @@ void do_force(FILE*                               fplog,
             nbv->atomdata_init_copy_x_to_nbat_x_gpu();
         }
 
-        nbv->atomdata_init_atomIndicesInv();
+        if (simulationWork.useGpuNonbonded)
+        {
+            nbv->atomdata_init_atomIndicesInv();
+        }
 
         if (simulationWork.useGpuBufferOps)
         {
